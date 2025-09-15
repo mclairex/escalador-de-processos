33f0b0c212a7f66c4c5874e4d30f2f35989dc287
Airtonrgj <seu.email@exemplo.com>
2025-09-15 00:56:28 -0300

    feat/cria atributos obrigatórios

---
+++ src/Scheduler.java
@@ -0,0 +1,18 @@
+public class Scheduler {
+    // Atributos obrigatórios conforme requisitos
+    private ListaDeProcessos listaAltaPrioridade;
+    private ListaDeProcessos listaMediaPrioridade;
+    private ListaDeProcessos listaBaixaPrioridade;
+    private ListaDeProcessos listaBloqueados;
+    private int contadorCiclosAltaPrioridade;
+    private int cicloAtual;
+
+    public Scheduler() {
+        this.listaAltaPrioridade = new ListaDeProcessos();
+        this.listaMediaPrioridade = new ListaDeProcessos();
+        this.listaBaixaPrioridade = new ListaDeProcessos();
+        this.listaBloqueados = new ListaDeProcessos();
+        this.contadorCiclosAltaPrioridade = 0;
+        this.cicloAtual = 0;
+    }
+}

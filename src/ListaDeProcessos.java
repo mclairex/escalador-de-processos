78ddcb40b2d8826e45f20a2c510f7b0ce8d016db
mclairex <mariasousaclara2019@gmail.com>
2025-09-16 00:21:50 -0300

    feat/cria logica para remover processo por id

--- src/ListaDeProcessos.java
+++ src/ListaDeProcessos.java
@@ -23,6 +23,51 @@ public class ListaDeProcessos {
         tamanho++;
     }
 
+
+        public boolean remover(int id) {
+        if (cabeca == null) return false;
+
+        NodeProcesso atual = cabeca;
+        NodeProcesso anterior = cauda;
+
+        do {
+            if (atual.getProcesso().getId() == id) {
+                if (atual == cabeca) {
+                    cabeca = cabeca.getProximo();
+                    cauda.setProximo(cabeca);
+                } else if (atual == cauda) {
+                    cauda = anterior;
+                    cauda.setProximo(cabeca);
+                } else {
+                    anterior.setProximo(atual.getProximo());
+                }
+                tamanho--;
+                if (tamanho == 0) {
+                    cabeca = null;
+                    cauda = null;
+                }
+                return true;
+            }
+            anterior = atual;
+            atual = atual.getProximo();
+        } while (atual != cabeca);
+
+        return false;
+    }
+
+    public void listar() {
+        if (cabeca == null) {
+            System.out.println("Lista vazia!");
+            return;
+        }
+
+        NodeProcesso atual = cabeca;
+        do {
+            System.out.println(atual.getProcesso());
+            atual = atual.getProximo();
+        } while (atual != cabeca);
+    }
+
     public int getTamanho() {
         return tamanho;
     }
